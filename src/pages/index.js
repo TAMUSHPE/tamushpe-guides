6749e29a8492a2d8b1a3734955ff5e3f92233808
jonathan343 <jonathangee09@gmail.com>
2021-09-24 15:27:10 -0500

    home page button

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,8 +16,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Learn Python
+            to="docs/personal-website/Purpose/introduction">
+            Create Personal Website
           </Link>
         </div>
       </div>
