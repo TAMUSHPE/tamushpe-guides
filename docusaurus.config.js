f9ea8a17a094d12688709edb63a034778e2add72
jonathan343 <jonathangee09@gmail.com>
2022-01-26 12:45:37 -0600

    baseURL issue

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -2,8 +2,8 @@
 module.exports = {
   title: 'tamuSHPE Guides',
   tagline: 'An inclusive environemnt for individuals who are passionate about using technology to improve themselves and the life of others.',
-  url: 'https://tech.tamushpe.org',
-  baseUrl: '/tamushpe-guides/',
+  url: 'https://tech.tamushpe.org/',
+  baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
